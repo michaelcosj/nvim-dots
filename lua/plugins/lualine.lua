ebacb6d0674517cc6d0fd1e7e81f352eb6aad76a
michael <michaelcosj@proton.me>
2024-03-15 18:27:36 +0100

    fix lua line not showing diagnostics

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,11 +1,20 @@
 return {
     'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    dependencies = { 'nvim-tree/nvim-web-devicons', 'cbochs/grapple.nvim' },
     opts = {
         options = {
             theme = 'nightfly',
             component_separators = { left = '|', right = '|' },
             section_separators = { left = '', right = '' },
+        },
+        sections = {
+            lualine_b = {
+                "branch", "diff", "diagnostics",
+                {
+                    require("grapple").statusline,
+                    cond = require("grapple").exists
+                }
+            }
         }
     }
 }
